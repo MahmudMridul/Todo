94634ed327f9578e110a237901c44721868d9924
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-24 15:55:44 +0600

    Add models

---
+++ TodoAPI/Models/ApiResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace TodoAPI.Models
+{
+    public class ApiResponse
+    {
+        public object? Data { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = false;
+        public string? Message { get; set; }
+    }
+}
---
+++ TodoAPI/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoAPI.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? Comment { get; set; }
+        public DateTime CreatedDate { get; set;}
+        public DateTime UpdatedDate { get; set;}
+        public DateTime Deadline { get; set;}
+        public bool IsCompleted { get; set;}
+    }
+}
