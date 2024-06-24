0c5aa895a714ae5c30d2b81f73b9f73afd1321ba
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-24 17:15:18 +0600

    Add validation class

---
+++ TodoAPI/Utils/ItemValidation.cs
@@ -0,0 +1,46 @@
+﻿using TodoAPI.Models.DTOs;
+
+namespace TodoAPI.Utils
+{
+    public static class ItemValidation
+    {
+        public static bool IsValidItem(ItemDTO item, out string message)
+        {
+            if(item == null)
+            {
+                message = "Item is empty";
+                return false;
+            }
+            if(string.IsNullOrWhiteSpace(item.Title))
+            {
+                message = "Invalid title given";
+                return false;
+            }
+            if(!string.IsNullOrEmpty(item.Description) && item.Description.Trim().Length == 0)
+            {
+                message = "Only white space given";
+                return false;
+            }
+            if (!string.IsNullOrEmpty(item.Comment) && item.Comment.Trim().Length == 0)
+            {
+                message = "Only white space given";
+                return false;
+            }
+            if(item.Deadline == null)
+            {
+                message = "Invalid deadline given";
+                return false;
+            }
+
+            DateTime minDate = DateTime.Now.AddMinutes(5);
+            if(item.Deadline < minDate)
+            {
+                message = "Deadline is too short";
+                return false;
+            }
+
+            message = "Success";
+            return true;
+        }
+    }
+}
