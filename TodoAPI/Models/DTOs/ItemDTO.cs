0e81b09f60ab196ab784a8cc0a1451b812ec3953
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-24 17:14:57 +0600

    Add item dto

---
+++ TodoAPI/Models/DTOs/ItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoAPI.Models.DTOs
+{
+    public class ItemDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? Comment { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+}
