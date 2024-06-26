2fbb8960a1baaaea727411f9d97f789d721b87a1
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-26 11:33:45 +0600

    Add date formatting function

--- todo-ui/src/todoUtils.js
+++ todo-ui/src/todoUtils.js
@@ -3,3 +3,15 @@ const api = process.env.REACT_APP_API;
 export const urls = {
    allTodos: `${api}Item`,
 };
+
+export function formatDateString(dateString) {
+   const date = new Date(dateString);
+
+   const hours = date.getHours().toString().padStart(2, '0');
+   const minutes = date.getMinutes().toString().padStart(2, '0');
+   const day = date.getDate();
+   const month = date.toLocaleString('default', { month: 'long' });
+   const year = date.getFullYear();
+
+   return `${hours}:${minutes} ${month} ${day}, ${year}`;
+}
