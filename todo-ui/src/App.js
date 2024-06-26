3cb9c32b3cc83bcf039395f92b54e38cf5baccb4
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-26 11:34:17 +0600

    Add ListItem

--- todo-ui/src/App.js
+++ todo-ui/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Header from './components/Header'
+import ListItem from './components/ListItem'
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllTodos } from './todoSlice';
 
 export default function App() {
+   const dispatch = useDispatch();
+   const states = useSelector(store => store.todo);
+   const { todos } = states;
+   useEffect(() => {
+      dispatch(getAllTodos());
+   }, []);
    return (
-      <div className='text-3xl font-bold'>App</div>
+      <main className='container p-2'>
+         <Header headerText={"Todos"} />
+         {todos.map((item, index) => {
+            const { title, description, comment, deadline } = item;
+            return (
+               <ListItem key={index} title={title} desc={description} comm={comment} deadline={deadline} />
+            );
+         })}
+      </main>
    )
 }
--- todo-ui/src/components/ListItem.jsx
+++ todo-ui/src/components/ListItem.jsx
@@ -1,20 +1,16 @@
 import React from "react";
+import { formatDateString } from "../todoUtils";
 
 export default function ListItem({ title, desc, comm, deadline }) {
    return (
-      <div className="border-2 rounded-2xl p-4 border-gray-400">
-         <div className="text-xl font-semibold mb-2">List Title</div>
+      <div className="my-5 border-2 rounded-2xl p-4 border-gray-400">
+         <div className="text-xl font-semibold mb-2">{title}</div>
+         <div className="mb-1">{desc}</div>
+         <div className="text-gray-500 mb-1">{comm}</div>
          <div className="mb-1">
-            List description. Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Odio, molestiae quaerat provident qui necessitatibus
-            laudantium deleniti error obcaecati maxime! Quos illum earum
-            architecto molestias animi dolor consequatur debitis quae fuga.
+            <span className="text-rose-800 font-semibold">Due:</span>{" "}
+            {formatDateString(deadline)}
          </div>
-         <div className="text-gray-500 mb-1">
-            Comment. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Odio.
-         </div>
-         <div className="mb-1">Deadline</div>
          <hr className="mb-4"></hr>
          <button className="w-16 p-1 bg-gray-800 rounded-md text-white font-semibold">
             Edit
