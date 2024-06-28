32d93b3042f95ef2ad2155726be62627a39ac3d7
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-06-28 11:39:37 +0600

    Add edit page content

--- todo-ui/src/pages/EditItem.jsx
+++ todo-ui/src/pages/EditItem.jsx
@@ -1,5 +1,39 @@
 import React from "react";
 
 export default function EditItem() {
-   return <div>EditItem</div>;
+   return (
+      <main className="container p-2 flex">
+         <div className="grid grid-cols-6 gap-y-5 mt-10">
+            <label className="col-span-2 text-xl">Title *</label>
+            <input
+               type="text"
+               required
+               className="col-span-4 border-2 w-80 h-8 rounded-md text-xl"
+            />
+
+            <label className="col-span-2 text-xl">Description</label>
+            <textarea
+               type="text"
+               className="col-span-4 border-2 w-80 h-20 rounded-md text-xl"
+            />
+
+            <label className="col-span-2 text-xl">Comment</label>
+            <textarea
+               type="text"
+               className="col-span-4 border-2 w-80 h-20 rounded-md text-xl"
+            />
+
+            <label className="col-span-2 text-xl">Due Date *</label>
+            <input
+               type="date"
+               required
+               className="col-span-4 border-2 w-80 h-8 rounded-md text-xl"
+            />
+
+            <button className="w-20 p-1 bg-gray-800 rounded-md text-white font-semibold">
+               Save
+            </button>
+         </div>
+      </main>
+   );
 }
