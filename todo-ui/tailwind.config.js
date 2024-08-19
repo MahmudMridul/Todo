be92dc4cad372e609a4ddaec2465a1843de1e962
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-08-19 16:39:40 +0600

    Add custom screens

--- todo-ui/tailwind.config.js
+++ todo-ui/tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-      "./src/**/*.{js,jsx}"
-   ],
+   content: ["./src/**/*.{js,jsx}"],
    theme: {
+      screens: {
+         mob: "425px",
+         tabS: "600px",
+         tabL: "768px",
+         lap: "1024px",
+         big: "1200px",
+      },
       container: {
          center: true,
       },
       extend: {},
    },
    plugins: [],
-}
-
+};
